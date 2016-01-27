ef28610c1a292f4ff2df5cafeec929854cf110cf
manjarb <manjarb4@gmail.com>
2016-01-28 01:52:05 +0800

    updatee

---
+++ app/main.rb
@@ -0,0 +1,23 @@
+require 'pathname'
+
+APP_ROOT = Pathname.new(File.dirname(File.expand_path(__FILE__)))
+
+require APP_ROOT.join('models', 'representative')
+require APP_ROOT.join('models', 'senator')
+
+senators = Senator.new
+representatives = Representative.new
+
+# # senators.state('NY').inspect
+# # representatives.state('NY').inspect
+# senators.gender('f')
+# representatives.gender('f')
+
+#state_list =
+senators_list = senators.return_state_list
+representatives_list = representatives.return_state_list
+i = 0
+senators_list.each { |value|
+	puts "#{value[0]}: #{value[1].length} Senators, #{representatives_list[value[0]].length} Representative(s)"
+	i += 1
+}
--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,14 +1,49 @@
 require 'csv'
+require_relative '../app/models/representative'
+require_relative '../app/models/senator'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
+    i = 0
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
-      row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
-        # TODO: end
+      person_hash = {
+                      :name => "#{row['firstname']} #{row['middlename']} #{row['lastname']}",
+                      :phone => row['phone'],
+                      :fax => row['fax'],
+                      :website => row['website'],
+                      :email => row['webform'],
+                      :gender => row['gender'],
+                      :birthday => row['birthdate'],
+                      :twitter_id => row['twitter_id'],
+                      :active => row['in_office'],
+                      :party => row['party'],
+                      :state => row['state']
+                    }
+      print person_hash
+      puts ""
+
+      if row['title'].to_s.downcase == "rep"
+        Representative.create!(person_hash)
+        #puts "made rep"
+      elsif row['title'].to_s.downcase == "sen"
+        Senator.create!(person_hash)
+        #puts "made sen"
       end
+      
+      # row.each do |field, value|
+      #   # TODO: begin
+      #   print field
+      #   puts ""
+      #   print value
+      #   puts ""
+
+      #   person_hash = {:name => }
+
+      #   #raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+
+      #   # TODO: end
+      # end
     end
   end
 end
@@ -23,3 +58,6 @@ end
 # rescue NotImplementedError => e
 #   $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
 # end
+
+filename = '../db/data/legislators.csv'
+SunlightLegislatorsImporter.import(filename)
